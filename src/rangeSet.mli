082c3100a1c5d0f635611e00c80ab7201151df6a
ZAN DoYe <357626+kandu@users.noreply.github.com>
2018-11-01 16:41:13 +0800

    init

---
+++ src/rangeSet.mli
@@ -0,0 +1,164 @@
+(** Sets over ordered ranges
+
+  This module implements a set data structure.
+
+  The elements are represented as ranges, and is therefore efficient for
+  large set of adjacent elements.
+*)
+
+module type OrderedType =
+  sig
+    type t
+    (** The type of the set elements. *)
+
+    val compare : t -> t -> int
+    (** A total ordering function over the set elements.
+        This is a two-argument function [f] such that
+        [f e1 e2] is zero if the elements [e1] and [e2] are equal,
+        [f e1 e2] is strictly negative if [e1] is smaller than [e2],
+        and [f e1 e2] is strictly positive if [e1] is greater than [e2].
+        Example: a suitable ordering function is the generic structural
+        comparison function {!Pervasives.compare}. *)
+
+    val to_string : t -> string
+    (** [to_string t] is the written representation of [t]. *)
+  end
+(** Input signature of the functor {!RangeSet.Make}. *)
+
+module type S =
+  sig
+    type elt
+    (** The type of the set elements. *)
+
+    type t
+    (** The type of the sets. *)
+
+    type point =
+      | Inc of elt (** including endpoint *)
+      | Exc of elt (** excluding endpoint *)
+
+    (** The type of the point of a range.
+        A point can be either closed or open, respectively [Inc elt] or [Exc elt] where [elt] is of type [elt] *)
+
+    type range = { start : point; stop : point; }
+    (** The type of the range.
+      A range consists of [start] and [end] endpoint.
+     *)
+
+    val point_to_string : point -> string
+    (** [point_to_string point] is the written representation of [point]. *)
+
+    val range_to_string : range -> string
+    (** [range_to_string range] is the written representation of [range]. *)
+
+    val to_string : t -> string
+    (** [to_string set] is the written representation of [set]. *)
+
+    val empty : t
+    (** The empty set. *)
+
+    val is_empty : t -> bool
+    (** [is_empty t] tests whether [t] is empty or not. *)
+
+    val mem : elt -> t -> bool
+    (** [mem x s] tests whether [x] belongs to the set [s]. *)
+
+    val add : elt -> t -> t
+    (** [add x s] returns a set containing all elements of [s],
+      plus [x]. If [x] was already in [s], [s] is returned unchanged. *)
+
+    val singleton : elt -> t
+    (** [singleton x] returns the one-element set containing only [x]. *)
+
+    val of_point : point -> point -> t
+    (** [of_point p1 p2] returns the set containing a range from p1 to p2.
+      *)
+
+    val of_range : range -> t
+    (** [of_range r] returns the set containing only the [range]. *)
+
+    val of_ranges : range list -> t
+    (** [of_ranges rs] returns the set containing all the ranges in [rs]. *)
+
+    val remove : elt -> t -> t
+    (** [remove x s] returns a set containing all elements of s, except x. *)
+
+    val merge : range -> t -> t
+    (** [merge r s] returns a set containing all elements of s
+      , plus range [r]. *)
+
+    val unmerge : range -> t -> t
+    (** [unmerge r s] returns a set containing all elements of s, except those in range [r]. *)
+
+    val cover : range -> t -> t
+    (** [cover r s] returns a set containing elements both belongs to range [r] and set [s]. *)
+
+    val union : t -> t -> t
+    (** Set union. *)
+
+    val diff : t -> t -> t
+    (** Set difference: [diff s1 s2] contains the elements of [s1]
+       that are not in [s2]. *)
+
+    val inter : t -> t -> t
+    (** Set intersection. *)
+  end
+(** Output signature of the functor {!RangeSet.Make}. *)
+
+module Make(Ord: OrderedType): S with type elt = Ord.t
+(** Functor building an implementation of the set structure
+   given a totally ordered type. *)
+
+
+module type OrderedType_Discrete =
+  sig
+    include OrderedType
+
+    val pred : t -> t (** predecessor *)
+    val succ : t -> t (** successor *)
+  end
+(** Input signature of the functor {!RangeSet.Make_Discrete}. *)
+
+module type S_Discrete =
+  sig
+    include S
+
+    val min_elt: t -> elt
+    (** Return the smallest element of the given set
+       (with respect to the [Ord.compare] ordering), or raise
+       [Not_found] if the set is empty. *)
+
+    val min_elt_opt: t -> elt option
+    (** Return the smallest element of the given set
+        (with respect to the [Ord.compare] ordering), or [None]
+        if the set is empty. *)
+
+    val max_elt: t -> elt
+    (** Return the largest element of the given set
+      (with respect to the [Ord.compare] ordering), or raise
+      [Not_found] if the set is empty. *)
+
+    val max_elt_opt: t -> elt option
+    (** Return the largest element of the given set
+      (with respect to the [Ord.compare] ordering), or [None]
+      if the set is empty. *)
+
+    val iter: (elt -> unit) -> t -> unit
+    (** [iter f s] applies [f] in turn to all elements of [s].
+      The elements of [s] are presented to [f] in increasing order
+      with respect to the ordering over the type of the elements. *)
+
+    val elements: t -> elt list
+    (** Return the list of all elements of the given set.
+        The returned list is sorted in increasing order
+        with respect to the ordering [Ord.compare],
+        where Ord is the argument given to [Set.Make_Discrete]. *)
+
+  end
+(** Output signature of the functor {!RangeSet.Make_Discrete}. *)
+
+
+
+module Make_Discrete(Ord: OrderedType_Discrete): S_Discrete with type elt = Ord.t
+(** Functor building an implementation of the set structure
+   given a totally ordered type. *)
---
+++ test/test.ml
@@ -0,0 +1,202 @@
+open RangeSet
+
+let%test_module _= (module struct
+  module Int=
+    struct
+      type t= int
+      let compare= compare
+      let to_string= string_of_int
+      let pred n= n - 1
+      let succ n= n + 1
+    end
+
+  module IS = Make_Discrete(Int)
+
+  let s1= IS.of_point (Exc 1) (Exc 7)
+  let s2= IS.of_point (Inc 1) (Inc 7)
+
+  let%test "s1 mem 1"= not (IS.mem 0 s1)
+  let%test "s1 mem 2"= not (IS.mem 1 s1)
+  let%test "s1 mem 3"= not (IS.mem 7 s1)
+  let%test "s1 mem 4"= not (IS.mem 8 s1)
+
+  let%test "s2 mem 1"= not (IS.mem 0 s2)
+  let%test "s2 mem 2"= IS.mem 1 s2
+  let%test "s2 mem 3"= IS.mem 7 s2
+  let%test "s2 mem 4"= not (IS.mem 8 s2)
+
+  let s_add= IS.empty
+  let s_add= IS.add 1 s_add
+  let%expect_test "add 1"= s_add
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 1) ]"]
+  let s_add= IS.add 2 s_add
+  let%expect_test "add 2"= s_add
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 1); (Inc 2, Inc 2) ]"]
+  let%expect_test "add 3"= IS.of_point (Inc 1) (Exc 2) |> IS.add 2
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 2) ]"]
+  let%expect_test "add 4"= IS.of_point (Exc 1) (Inc 2) |> IS.add 1
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 2) ]"]
+  let%expect_test "add 5"=
+    IS.union
+      (IS.of_point (Exc 1) (Exc 2))
+      (IS.of_point (Exc 2) (Exc 3))
+      |> IS.add 2
+      |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Exc 3) ]"]
+
+  let s_remove= IS.of_point (Inc 1) (Inc 3)
+  let s_remove= IS.remove 1 s_remove
+  let%expect_test "remove 1"= 
+    s_remove |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Inc 3) ]"]
+  let s_remove= IS.remove 3 s_remove
+  let%expect_test "remove 2"= 
+    s_remove |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Exc 3) ]"]
+  let s_remove= IS.remove 2 s_remove
+  let%expect_test "remove 3"= 
+    s_remove |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Exc 2); (Exc 2, Exc 3) ]"]
+  let s_remove= IS.remove 10 s_remove
+  let%expect_test "remove 4"= 
+    s_remove |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Exc 2); (Exc 2, Exc 3) ]"]
+
+  let%expect_test "of_point 1"= IS.of_point (Exc 1) (Inc 1)
+    |> IS.to_string |> print_endline;
+    [%expect "[  ]"]
+  let%expect_test "of_point 2"= IS.of_point (Inc 1) (Exc 1)
+    |> IS.to_string |> print_endline;
+    [%expect "[  ]"]
+  let%expect_test "of_point 3"= IS.of_point (Inc 2) (Inc 1)
+    |> IS.to_string |> print_endline;
+    [%expect "[  ]"]
+  let%expect_test "of_point 4"= IS.of_point (Inc 1) (Inc 1)
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 1) ]"]
+  let%expect_test "of_point 5"= IS.of_point (Inc 1) (Inc 2)
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 2) ]"]
+
+  let%expect_test "of_range 1"= IS.of_range { start= Exc 1; stop= Inc 1 }
+    |> IS.to_string |> print_endline;
+    [%expect "[  ]"]
+  let%expect_test "of_range 2"= IS.of_range { start= Inc 1; stop= Exc 1 }
+    |> IS.to_string |> print_endline;
+    [%expect "[  ]"]
+  let%expect_test "of_range 3"= IS.of_range { start= Exc 1; stop= Exc 1 }
+    |> IS.to_string |> print_endline;
+    [%expect "[  ]"]
+  let%expect_test "of_range 1"= IS.of_range { start= Inc 1; stop= Inc 1 }
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 1) ]"]
+  let%expect_test "of_range 1"= IS.of_range { start= Inc 1; stop= Inc 2 }
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 2) ]"]
+
+  let%expect_test "of_ranges 1"= IS.of_ranges [
+    { start= Exc 1; stop= Exc 2 };
+    { start= Inc 1; stop= Inc 2 }; ]
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Inc 2) ]"]
+  let%expect_test "of_ranges 2"= IS.of_ranges [
+    { start= Exc 1; stop= Exc 2 };
+    { start= Inc 2; stop= Inc 3 }; ]
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Inc 3) ]"]
+  let%expect_test "of_ranges 3"= IS.of_ranges [
+    { start= Exc 2; stop= Exc 3 };
+    { start= Inc 1; stop= Inc 2 }; ]
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 1, Exc 3) ]"]
+  let%expect_test "of_ranges 4"= IS.of_ranges [
+    { start= Exc 1; stop= Exc 3 };
+    { start= Exc 7; stop= Exc 9 };
+    { start= Exc 2; stop= Exc 8 }; ]
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Exc 9) ]"]
+
+
+  let s3= IS.of_ranges [
+    { start= Exc 1; stop= Inc 4 };
+    { start= Inc 7; stop= Exc 9 } ;
+    { start= Inc 13; stop= Exc 15 };
+    { start= Exc 19; stop= Inc 21 }; ]
+
+  let%expect_test "union"=
+    let test s1 s2=
+      IS.union s1 s2
+        |> IS.to_string |> print_endline;
+      [%expect "[ (Inc -5, Inc -4); (Exc 1, Inc 4); (Inc 7, Inc 10); (Inc 13, Inc 21) ]"]
+    in
+    let s4= IS.of_ranges [
+      { start= Inc (-5); stop= Inc (-4) };
+      { start= Inc 2; stop= Inc 3 };
+      { start= Inc 9; stop= Inc 10 };
+      { start= Inc 15; stop= Inc 19 }; ]
+    in
+    test s3 s4;
+    test s4 s3
+
+  let%expect_test "unmerge1"=
+    IS.unmerge { start= Exc 7; stop= Inc 8 } s3
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Inc 4); (Inc 7, Inc 7); (Exc 8, Exc 9); (Inc 13, Exc 15); (Exc 19, Inc 21) ]"]
+  let%expect_test "unmerge2"=
+    IS.unmerge { start= Inc 6; stop= Inc 16 } s3
+    |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Inc 4); (Exc 19, Inc 21) ]"]
+  let%expect_test "unmerge3"=
+    IS.unmerge { start= Inc 8; stop= Inc 14 } s3
+      |> IS.to_string |> print_endline;
+    [%expect "[ (Exc 1, Inc 4); (Inc 7, Exc 8); (Exc 14, Exc 15); (Exc 19, Inc 21) ]"]
+
+  let%expect_test "diff"=
+    let s4= IS.of_ranges [
+      { start= Inc 0; stop= Inc 4 };
+      { start= Exc 7; stop= Inc 8 };
+      { start= Inc 9; stop= Exc 10 };
+      { start= Inc 12; stop= Exc 14 };
+      ]
+    in
+    IS.diff s3 s4 |> IS.to_string |> print_endline;
+    [%expect "[ (Inc 7, Inc 7); (Exc 8, Exc 9); (Inc 14, Exc 15); (Exc 19, Inc 21) ]"]
+
+  let%test "inter1"= IS.inter s1 s2 = s1
+  let%test "inter2"= IS.inter s2 s1 = s1
+  let%expect_test "inter3"=
+    let test s1 s2= IS.inter s1 s2
+      |> IS.to_string |> print_endline;
+      [%expect "[ (Exc 1, Inc 4); (Exc 7, Inc 8); (Inc 13, Exc 14) ]"]
+    in
+    let s4= IS.of_ranges [
+      { start= Inc 0; stop= Inc 4 };
+      { start= Exc 7; stop= Inc 8 };
+      { start= Inc 9; stop= Exc 10 };
+      { start= Inc 12; stop= Exc 14 };
+      ]
+    in
+    test s3 s4;
+    test s4 s3
+
+  let s5= IS.of_ranges [
+    { start= Exc 1; stop= Exc 4 };
+    { start= Exc 5; stop= Inc 7 } ;
+    { start= Inc 8; stop= Exc 10 };
+    { start= Inc 11; stop= Inc 13 };
+    ]
+
+  let%expect_test "iter"=
+    IS.iter (Printf.printf " %d") s5;
+    [%expect "2 3 6 7 8 9 11 12 13"]
+
+  let%expect_test "elements"= IS.elements s5
+    |> List.iter (Printf.printf " %d");
+    [%expect "2 3 6 7 8 9 11 12 13"]
+    
+end)
+
